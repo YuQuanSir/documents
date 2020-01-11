a19028c94ca00bed63fd580c4e94c3961f8b9669
YuQuanSir <2067299793@qq.com>
2020-01-11 23:12:06 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YuQuabSir/documents.git master:gh-pages
+git push -f git@github.com:YuQuanSir/documents.git master:gh-pages
 
 cd -
